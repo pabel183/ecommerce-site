137547238dbb38b806502e09ce57dc9ec5f6120d
pabel183 <jhpabel183@gmail.com>
2024-10-06 20:57:38 +0600

    fix: storagekepper deshboard change into deshboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -23,7 +23,7 @@ const DashboardPage:React.FC<DashboardPageProps>=async({
     return(
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <Heading title="Storagekeeper Dashboard" description="Overview of your Storagekeeper"/>
+                <Heading title="Dashboard" description="Overview of your Storagekeeper"/>
                 <Separator />
                 <div className="grid gap-4 grid-cols-3">
                     <Card>
